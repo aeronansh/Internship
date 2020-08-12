4e299be5b3d31dba2f8fce1205b20f1cdf4163ca
Aman Sharma <53836272+aeronansh@users.noreply.github.com>
2020-08-12 13:12:31 +0530

    Update CollectionUtils.java

--- Collection Usage/utils/CollectionUtils.java
+++ Collection Usage/utils/CollectionUtils.java
@@ -6,12 +6,9 @@ import com.app.iacsd.Student;
 import static utils.ValidationRules.*;
 
 public class CollectionUtils {
-	/*
-	 * Add a static method "populateData" to return populated(sample data -- 4) AL *
-	 * of students.
-	 */
+	
 	public static ArrayList<Student> populateData() throws Exception {
-		// create empty AL
+		//  empty AL
 		ArrayList<Student> l1 = new ArrayList<>();
 		l1.add(new Student("dbda4", "abc3", 75,validateCourse("dbda"), parseDate("1-2-1996")));
 		l1.add(new Student("dbda3", "abc5", 65, validateCourse("dmc"), parseDate("11-2-1996")));
